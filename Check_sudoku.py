a30512bf5403587d217bbf295b89e012897012f0
juadel <juadel@hotmail.com>
2019-04-21 22:01:17 -0600

    Update Check_sudoku.py

--- Check_sudoku.py
+++ Check_sudoku.py
@@ -2,7 +2,7 @@ def check_sudoku(sudo):
     
     # Checking if matrix is valid
     columns = len(sudo)
-    
+    print(sudo)
     for i in sudo:
        # print(len(i))
         #print(i)
@@ -15,33 +15,55 @@ def check_sudoku(sudo):
                 return False
      
     # Checking numbers in order per line
-    a= list(range(1,columns+1))
+    
     for i in sudo:
-      print(i)
-      print(a)
+      #print(i)
+      #print(a)
       a = list(range(1,columns+1))
       for n in i:
           if n in a:
-              print(n)
-              print(a)
-              print(a.index(n))
+              #print(n)
+              #print(a)
+              #print(a.index(n))
               a.pop(a.index(n))
-          #else:
-           #    False
+          else:
+              print(" Sudoku is not Correct ") 
+              return False
    
-     # Checking numbers in order per columns     
-           
-    print ("Sudoku Completed")
+     # Checking numbers in order per columns  
+    a= list(range(1,columns+1))
+    num2 =0
+    while num2 != columns-1:
+        num = 0
+        num2 +=1
+        a= list(range(1,columns+1))
+        while num != columns-1:
+            
+            if sudo[num][num2] in a:
+                #print(num, num2)
+
+                #print(sudo[num][num2])
+                a.pop(a.index(sudo[num][num2]))
+                #print(a)
+                num += 1
+                #print(num)
+            else:
+                print(" Sudoku is not Correct ") 
+                return False
+      
+
+    print ("Sudoku is Correct")
+    return True
 
 
-test1 = [[2,1,3,4],[2,3,1,4],[3,1,2,4],[2,4,3,1]]
+test1 = [[2,1,3,4],[1,3,1,4],[3,1,2,4],[4,4,3,1]]
 test2 = [[1,2,3,4],  
              [2,3,1,3], 
              [3,1,2,3],
              [4,4,4,4]]
 
 test3 = [[1,2,3,4],
-             [2,3,1,4],
+             [2,3,4,1],
              [4,1,2,3],
              [3,4,1,2]]
 
@@ -58,11 +80,11 @@ test5 = [['a','b','c'],
 test6 = [ [1, 1.5],
                [1.5, 1]]
     
-#check_sudoku(test1)
+check_sudoku(test1)
 
-#check_sudoku(test2)
+check_sudoku(test2)
 check_sudoku(test3)
-#check_sudoku(test4)
+check_sudoku(test4)
 check_sudoku(test5)
-#check_sudoku(test6)
+check_sudoku(test6)
 
