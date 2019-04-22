7c43b3753d59595511f2a1e6c89b1f34e66bcbae
juadel <juadel@hotmail.com>
2019-04-21 20:32:25 -0600

    Update Check_sudoku.py

--- Check_sudoku.py
+++ Check_sudoku.py
@@ -1,22 +1,25 @@
 def check_sudoku(sudo):
     
-    n = len(sudo)
+    # Checking if matrix is valid
+    columns = len(sudo)
     
     for i in sudo:
        # print(len(i))
         #print(i)
-        if len(i) != n :
+        if len(i) != columns :
             print ("Matrix is not square")
             return False
         for n in i:
             if type(n) != int: 
                 print(" Matrix not contain Integers")
+                return False
      
-    a= list(range(1,n+1))
+    # Checking numbers in order per line
+    a= list(range(1,columns+1))
     for i in sudo:
       print(i)
       print(a)
-      a = list(range(1,n+1))
+      a = list(range(1,columns+1))
       for n in i:
           if n in a:
               print(n)
@@ -25,10 +28,13 @@ def check_sudoku(sudo):
               a.pop(a.index(n))
           #else:
            #    False
+   
+     # Checking numbers in order per columns     
+           
     print ("Sudoku Completed")
 
 
-test1 = [[2,1,3],[2,3,1],[3,1,2]]
+test1 = [[2,1,3,4],[2,3,1,4],[3,1,2,4],[2,4,3,1]]
 test2 = [[1,2,3,4],  
              [2,3,1,3], 
              [3,1,2,3],
@@ -52,11 +58,11 @@ test5 = [['a','b','c'],
 test6 = [ [1, 1.5],
                [1.5, 1]]
     
-check_sudoku(test1)
+#check_sudoku(test1)
 
-#ujcheck_sudoku(test2)
-#check_sudoku(test3)
+#check_sudoku(test2)
+check_sudoku(test3)
 #check_sudoku(test4)
-#check_sudoku(test5)
+check_sudoku(test5)
 #check_sudoku(test6)
 
